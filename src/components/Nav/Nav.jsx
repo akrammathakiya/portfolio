62e71d9d4e5fcaf2ff410b6ab9f848f28c3402e8
akrammathakiya <mathakiyaakram@gmail.com>
2025-06-29 09:40:48 +0530

    navbar complete

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Nav.css"
+import { Link } from 'react-scroll'
+const Nav = () => {
+  return (
+    <nav>
+      <h1>PORTFOLIO</h1>
+      <ul>
+        <Link to="home" activeClass='active' spy={true} smooth={true} duration={500}><li>Home</li></Link>
+        <Link to="about" activeClass='active' spy={true} smooth={true} duration={500}><li>About</li></Link>
+        <Link to="projects" activeClass='active' spy={true} smooth={true} duration={500}><li>Projects</li></Link>
+        <Link to="contact" activeClass='active' spy={true} smooth={true} duration={500}><li>Contact</li></Link>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
